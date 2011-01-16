846488945972ab09aebb86a7ddc97bdb90b85f89
Kevin Thompson <thompson.kevind@gmail.com>
2011-01-16 11:34:32 -0800

    Reworked plugin variables and modified settings.

--- jquery.sticky.js
+++ jquery.sticky.js
@@ -1,54 +1,97 @@
+
+
+// Sticky – Keeps elements fixed as the user scrolls.
 (function($){
 	$.fn.sticky = function( options ){
 		
 		var settings = {
-			'offset'			:	20,
-			'fixedClass'		:	'fixed',
-			'absoluteClass'		:	'absolute',
-			'placeholderClass'	:	'placeholder'
+			'offset'			: 20,
+			'placeholder'		: true,
+			'classes'			: {
+				'fixed'			: 'fixed',
+				'absolute'		: 'absolute',
+				'placeholder'	: 'placeholder',
+			}
 		};
 		
 		this.each(function() {
+			
 			if (options) { 
-				$.extend(settings,options);
+		    	$.extend(settings,options);
 			}
 			
-			var $stopper = $(settings.stopper);
-			var hasStopper = $stopper.length > 0 ? true : false;
-			var $sticky = $(this).css({
-				'width'	: $(this).width()
-			}).addClass(settings.absoluteClass);		
+			var $document = $(document);
+			var $window = $(window);
 			
-			var top = settings.offset - parseInt($(this).css('marginTop'));
-			var stickyHeight = $sticky.outerHeight();
-			var totalHeight = stickyHeight + parseInt($sticky.css('marginTop')) + parseInt($sticky.css('marginBottom'));
-			var $placeholder = $('<div class="' + settings.placeholderClass + '">').css({
-				'height': 1
-			}).insertAfter($sticky);
-			var $view = $(document);
-			var originalStickyTop = $sticky.offset().top - settings.offset;
-
-			var updatePosition = function(){
-				var viewTop = $view.scrollTop();
-				var bottomPosition = viewTop + top + totalHeight;
-				if (hasStopper) {
-					var stopperPosition = $stopper.offset().top - parseInt($stopper.css('marginTop'));
-					if (bottomPosition > stopperPosition) {
-						$sticky.removeClass(settings.absoluteClass).addClass(settings.fixedClass).css('top', top - (bottomPosition - stopperPosition));
+			var sticky = {
+			
+				'init'		: function($this){
+					
+					sticky.element = $this;
+			
+					if(settings.placeholder){
+						sticky.placeholder = sticky.element.clone().addClass(settings.classes.placeholder).css({
+							'visibility':'hidden'
+						}).insertAfter(sticky.element);
 					}
-				}
+					sticky.element.css({
+						'width'	: sticky.element.width()
+					}).addClass(settings.classes.absolute);
+					sticky.stopper = $(settings.stopper) || $(sticky.element.attr('data-stopper'))
+					sticky.units = {
+						'top'		: settings.offset - parseInt(sticky.element.css('marginTop')),
+						'height'	: sticky.element.outerHeight() + parseInt(sticky.element.css('marginTop')) + parseInt(sticky.element.css('marginBottom')),
+						'origin'	: sticky.element.offset().top - settings.offset
+					};
+					
+					sticky.update();
+					$window.bind('resize', function(){
+						sticky.update();
+					});
+					$document.bind('scroll', function () {
+						sticky.update();
+					});
+				},
+			
+				'update'	: function(){
+					if(settings.placeholder){
+						sticky.element.width(sticky.placeholder.width());
+					}
+					
+					sticky.units.doctop = $document.scrollTop();
 				
-				if (viewTop > originalStickyTop){
-					$sticky.removeClass(settings.absoluteClass).addClass(settings.fixedClass).css('top',top);
-				} else if ((viewTop <= originalStickyTop) && $sticky.hasClass(settings.fixedClass)) {
-					$sticky.removeClass(settings.fixedClass).addClass(settings.absoluteClass).css('top','auto');
+					if(sticky.element.outerHeight() < $window.height()){
+						sticky.units.bottom = sticky.units.top + sticky.units.doctop + sticky.units.height;
+						if (sticky.stopper.length > 0) {
+							sticky.units.stopper = sticky.stopper.offset().top - parseInt(sticky.stopper.css('marginTop'));
+						}
+						if (sticky.stopper.length > 0 && sticky.units.bottom > sticky.units.stopper) {
+							sticky.enable(sticky.units.top - (sticky.units.bottom - sticky.units.stopper));
+						} else if (sticky.units.doctop > sticky.units.origin){
+							sticky.enable();
+						} else if ((sticky.units.doctop <= sticky.units.origin) && sticky.element.hasClass(settings.classes.fixed)) {
+							sticky.disable();
+						}
+					}
+				},
+				
+				'enable'	: function(top){
+					top = top || sticky.units.top;
+					sticky.element
+						.removeClass(settings.classes.absolute)
+						.addClass(settings.classes.fixed)
+						.css('top',top);
+				},
+			
+				'disable'	: function(){
+					sticky.element
+						.removeClass(settings.classes.fixed)
+						.addClass(settings.classes.absolute)
+						.css('top','auto');
 				}
 			}
-
-			updatePosition();
-			$view.bind("scroll resize", function () {
-				updatePosition();
-			});
+			
+			sticky.init($(this));
 		});
 	};
 })(jQuery);
