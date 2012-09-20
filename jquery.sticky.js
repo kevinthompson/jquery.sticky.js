85ee5d47be7e6c5f54e71ad8e33eec496962a113
Kevin Thompson <kevin@kevinthompson.info>
2012-09-19 17:32:17 -0700

    Clean up formatting and revise a few conditionals.

--- jquery.sticky.js
+++ jquery.sticky.js
@@ -3,29 +3,29 @@
  * 
  * Keep interface elements fixed in place as the user scrolls.
  * 
- * Copyright (c) 2011 Kevin Thompson <http://kevinthompson.info>
+ * Copyright (c) 2012 Kevin Thompson <http://kevinthompson.info>
  * Dual licensed under the MIT and GPL licenses:
  * http://www.opensource.org/licenses/mit-license.php
  * http://www.gnu.org/licenses/gpl.html
  *
  * @author:   Kevin Thompson <kevin@kevinthompson.info>
  * @link:     https://github.com/kevinthompson/jquery.sticky.js
- * @version:  1.0
+ * @version:  1.1
 **/ 
 
-(function ($) {
-  $.fn.sticky = function (options ) {
-    
+;(function ($) {
+  $.fn.sticky = function (options) {
+      
     var settings = {
       
       // Basic Settings
-      'offset'      : 20,
-      'mode'        : 'fixed',
-      'stopper'     : '',
-      'speed'       : 500,
+      'offset'  : 20,
+      'mode'    : 'fixed',
+      'stopper' : '',
+      'speed'   : 500,
       
       // State Classes
-      'classes'     : {
+      'classes' : {
         'element'     : 'jquery-sticky-element',
         'start'       : 'jquery-sticky-start',
         'sticky'      : 'jquery-sticky-sticky',
@@ -34,66 +34,54 @@
       },
       
       // Events
-      'onStick'       : '', // When the element becomes sticky
-      'onStart'       : '', // When the element returns to the start
-      'onStop'        : ''  // When the element hits a stopper
+      'onStick' : '', // When the element becomes sticky
+      'onStart' : '', // When the element returns to the start
+      'onStop'  : ''  // When the element hits a stopper
       
     };
     
     this.each(function () {
       
-      if (options) { 
-        $.extend(settings,options);
-      }
-      
-      if ($(this).parent().hasClass(settings.classes.element)) {
-        return;
-      }
-      
-      var $document = $(document);
-      var $window = $(window);
-      
+      if (options) $.extend(settings,options);
+      if ($(this).parent().hasClass(settings.classes.element)) return;
+
       var sticky = {
       
-        'init'    : function ($this) {
+        'init': function ($this) {
           
-          // Instantiate Element and Assign Basic Class
+          // Determine Sticky Element, Mode and Units (Force Animate Mode in Internet Explorer)
           settings.mode   = ($.browser.msie && settings.mode == 'fixed' ? settings.mode = 'animate' : settings.mode);
-          
-          // Wrap Selected Element
-          $this.wrap('<div class="' + settings.classes.element + '" />');
-          sticky.element  = $this.parent();
-          
-          // Set Base Units
-          sticky.units = {
-            'start'   : sticky.element.offset().top
-          };
+          sticky.element  = $this.wrap('<div class="' + settings.classes.element + '" />').parent();
+          sticky.units    = { 'start': sticky.element.offset().top };
+          settings.states = [
+            settings.classes.start,
+            settings.classes.sticky,
+            settings.classes.stopped
+          ].join(' ');
           
           // Find Stopper
-          if(settings.stopper != '') {
-            var stoppers    = $(settings.stopper);
-            if( stoppers.length > 0 ){
-              var i = 0;
-              while(i < stoppers.length && typeof(sticky.stopper) == 'undefined'){
-                if( $(stoppers[i]).offset().top > sticky.element.offset().top + sticky.element.outerHeight() ){
+          if (settings.stopper != '') {
+            var stoppers = $(settings.stopper), margin;
+            if ( stoppers.length > 0 ) {
+              for (i = 0; i < stoppers.length && typeof(sticky.stopper) == 'undefined'; i++) {
+                if ( $(stoppers[i]).offset().top > sticky.element.offset().top + sticky.element.outerHeight(false) ) {
                   sticky.stopper = $(stoppers[i]);
                 }
-                i++;
               }
             }
           
             // Update Stop Position
             if(typeof(sticky.stopper) != 'undefined' && sticky.stopper.length > 0) {
-              var margin = (parseInt(sticky.stopper.css('margin-top')) != undefined ? parseInt(sticky.stopper.css('margin-top')) : 0);
+              margin = parseInt(sticky.stopper.css('margin-top')) || 0;
               sticky.units.stop = sticky.stopper.offset().top - margin;
             }
           }
           
           // Create Placeholder
           sticky.placeholder = sticky.element.clone().empty().attr('class',settings.classes.placeholder).css({
-            'opacity'     : 0,
-            'height'      : sticky.element.height()
-          }).insertAfter(sticky.element);
+            'opacity' : 0,
+            'height'  : sticky.element.height()
+          }).insertBefore(sticky.element);
           
           // Move Sticky Element
           sticky.element
@@ -108,26 +96,28 @@
             });
           
           // Bind Events
-          $window.bind('resize scroll', function () {
+          $(window).bind('resize scroll', function () {
             sticky.update();
           });
           sticky.update();
         },
       
-        'update'  : function () {
+        'update' : function () {
+
+          var margin;
 
           // Update Sticky Element CSS
           sticky.element.css({
-            'width' : sticky.placeholder.width(),
-            'left'  : sticky.placeholder.offset().left
+            'width': sticky.placeholder.width(),
+            'left': sticky.placeholder.offset().left
           });
           sticky.placeholder.css('height',sticky.element.height());
           sticky.units.start = sticky.placeholder.offset().top;
           
-          if((sticky.element.outerHeight() + settings.offset) < $window.height()) {
+          if((sticky.element.outerHeight(false) + settings.offset) < $(window).height()) {
             
             // Get Window Top
-            sticky.units.doctop = $document.scrollTop();
+            sticky.units.doctop = $(document).scrollTop();
           
             // Update Animated Position
             if(sticky.element.hasClass(settings.classes.sticky) && settings.mode == 'animate') {
@@ -136,107 +126,96 @@
           
             // Update Stop Position
             if(typeof(sticky.stopper) != 'undefined' && sticky.stopper.length > 0) {
-              var margin = (parseInt(sticky.stopper.css('margin-top')) != undefined ? parseInt(sticky.stopper.css('margin-top')) : 0);
+              margin = parseInt(sticky.stopper.css('margin-top')) || 0;
               sticky.units.stop = sticky.stopper.offset().top - margin;
             }
             
-            // Stop at stopper
+            // Stop at Stopper
             if (
               ! sticky.element.hasClass(settings.classes.stopped) 
               && typeof(sticky.stopper) != 'undefined' 
               && sticky.stopper.length > 0 
-              && (sticky.units.doctop + settings.offset + sticky.element.outerHeight()) >= sticky.units.stop
+              && (sticky.units.doctop + settings.offset + sticky.element.outerHeight(false)) >= sticky.units.stop
             ) {
-              sticky.stop(sticky.units.stop - sticky.element.outerHeight(),'stop');
+              // Stop
+              sticky.stop(sticky.units.stop - sticky.element.outerHeight(false),'stop');
 
-              if (typeof settings.onStop == 'function') {
-                settings.onStop();
-              }
-              
-            // Update Position
+              // Trigger `onStop` Callback
+              if (typeof settings.onStop == 'function') settings.onStop();  
             } 
+            
+            // Update Position
             else if (
               ! sticky.element.hasClass(settings.classes.sticky) 
               && sticky.units.doctop > (sticky.units.start - settings.offset)
-              && (typeof(sticky.stopper) == 'undefined' || sticky.stopper.length == 0 || (sticky.stopper.length > 0 && (sticky.units.doctop + settings.offset + sticky.element.outerHeight()) < sticky.units.stop))
+              && (typeof(sticky.stopper) == 'undefined' || sticky.stopper.length == 0 || (sticky.stopper.length > 0 && (sticky.units.doctop + settings.offset + sticky.element.outerHeight(false)) < sticky.units.stop))
             ) {
+              // Stick or Animate
               sticky.stick(settings.offset);
-              
               if(settings.mode == 'animate') sticky.animate(sticky.units.doctop + settings.offset);
 
-              if (typeof settings.onStick == 'function') {
-                settings.onStick();
-              }
+              // Trigger `onStick` Callback
+              if (typeof settings.onStick == 'function') settings.onStick();
             
             // Stop at starting position
             } 
-            else if (
-              sticky.units.doctop <= (sticky.units.start - settings.offset)
-            ) {
+            else if ( sticky.units.doctop <= (sticky.units.start - settings.offset) ) {
               sticky.stop(sticky.units.start,'start');
 
-              if (typeof settings.onStart == 'function') {
-                settings.onStart();
-              }
+              // Trigger `onStart` Callback
+              if (typeof settings.onStart == 'function') settings.onStart();
             }
-            
-          }else{
-            
-            // Reset to Start
-            if(!sticky.element.hasClass(settings.classes.start)) {
-              sticky.stop(sticky.units.start,'start');  
-            }
+          
+          // Reset to Start
+          } else if(!sticky.element.hasClass(settings.classes.start)) {
+            sticky.stop(sticky.units.start,'start');  
           }
         },
         
-        'animate' : function (top) {
+        'animate': function (top) {
           clearTimeout(sticky.timer);
           sticky.timer = setTimeout(function () {
             if(top >= sticky.units.stop) {
-              top = sticky.units.stop - sticky.element.outerHeight();
+              top = sticky.units.stop - sticky.element.outerHeight(false);
             }else if(top <= sticky.units.start) {
               top = sticky.units.start;
             }
             sticky.element.stop().animate({
-              'top' : top
+              'top': top
             },settings.speed);
           },100);
         },
         
-        'stick'   : function (top) {
+        'stick': function (top) {
           sticky.element
-            .removeClass(settings.classes.start)
-            .removeClass(settings.classes.stopped)
+            .removeClass(settings.states)
             .addClass(settings.classes.sticky);
           
           if(settings.mode == 'fixed') {
             sticky.element.css({
-              'top'   : top,
-              'position'  : 'fixed'
+              'top': top,
+              'position': 'fixed'
             });
           }
         },
       
-        'stop'  : function (top,location) {
-          var oldClass = (location == 'start' ? settings.classes.stopped : settings.classes.start);
-          var newClass = (location == 'stop'  ? settings.classes.stopped : settings.classes.start);
-          
+        'stop': function (top,location) {
           sticky.element
-            .removeClass(settings.classes.sticky)
-            .removeClass(oldClass)
-            .addClass(newClass);
-            
+            .removeClass(settings.states)
+            .addClass(location == 'start' ? settings.classes.start : settings.classes.stopped);
+          
           if(settings.mode == 'fixed') {
             sticky.element.css({
-              'top'   : top,
-              'position'  : 'absolute'
+              'top': top,
+              'position': 'absolute'
             });
           }else{
-            sticky.animate(location == 'start' ? sticky.units.start : sticky.units.stop - sticky.element.outerHeight());
+            sticky.animate(location == 'start' ? sticky.units.start : sticky.units.stop - sticky.element.outerHeight(false));
           }
         }
       }
       
+      // Initialize Sticky Behavior
       sticky.init($(this));
     });
   };
